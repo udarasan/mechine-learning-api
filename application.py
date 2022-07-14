b4396fc22b349846100083720bced8fdd7961f7b
udarasan <udarassanjeewa@gmail.com>
2022-07-14 21:10:32 +0530

    502 error

--- application.py
+++ application.py
@@ -1,15 +1,16 @@
+from email.mime import application
 from flask import Flask, jsonify, request
 import pickle
 import numpy as np
-app = Flask(__name__)
+application = Flask(__name__)
 
 
-@app.route("/")
+@application.route("/")
 def index():
     return "<p>Hello, World!</p>"
 
 
-@app.route("/login",methods=['GET'])
+@application.route("/login",methods=['GET'])
 def login():
     
     data=request.get_json(force=True)
@@ -22,9 +23,9 @@ def login():
 
     print(data.values())
     print(pred[0])
-    
+
     output=pred[0]
     return jsonify(output)
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    application.run(debug=True)
